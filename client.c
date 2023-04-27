7a4cc9da282a11e654a0bc06943f8b64cbf27f84
andresdlRoca <andresdelaroca11@gmail.com>
2023-04-27 16:44:56 -0600

    Definicion del layout de las funciones principales planeadas

---
+++ client.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+// Global Variables
+
+void receive_msg_handle() { // Manejara los mensajes entrantes
+
+
+}
+
+void send_msg_handle() { // Manejara los mensajes enviados
+
+
+}
+
+void send_private_msg_handle(){ // Manejara los mensajes privados enviados
+
+}
+
+void send_status_change() { // Manejara el cambio de estado del usuario 
+
+
+}
+
+void get_users_in_chat() { // Obtiene un listado de los usuarios y su informacion
+
+}
+
+int main () {
+
+}
---
+++ server.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+void global_message_handler() { // Maneja los mensajes globales enviados a todos los clientes
+
+
+}
+
+void client_handle() { // Maneja a los clientes
+
+
+}
+
+void register_user() { // Maneja el registro de los clientes
+
+
+}
+
+void free_user() { // Liberacion de usuarios
+
+
+}
+
+int main() {
+
+
+}
+

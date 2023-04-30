ec84b52aab88528b3753a939cde490574e322266
andresdlRoca <andresdelaroca11@gmail.com>
2023-04-30 15:09:27 -0600

    Implementacion inicial chat y server

--- client.c
+++ client.c
@@ -8,32 +8,133 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+//#include <chat.pb-c.h>
 
-// Global Variables
+#define LENGTH 2048
 
-void receive_msg_handle() { // Manejara los mensajes entrantes
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
 
-
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
 }
 
-void send_msg_handle() { // Manejara los mensajes enviados
-
-
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
 }
 
-void send_private_msg_handle(){ // Manejara los mensajes privados enviados
-
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
 }
 
-void send_status_change() { // Manejara el cambio de estado del usuario 
+void send_msg_handler() {
+  char message[LENGTH] = {};
+	char buffer[LENGTH + 32] = {};
 
+  while(1) {
+  	str_overwrite_stdout();
+    fgets(message, LENGTH, stdin);
+    str_trim_lf(message, LENGTH);
 
+    if (strcmp(message, "exit") == 0) {
+			break;
+    } else {
+      sprintf(buffer, "%s: %s\n", name, message);
+      send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+		bzero(message, LENGTH);
+    bzero(buffer, LENGTH + 32);
+  }
+  catch_ctrl_c_and_exit(2);
 }
 
-void get_users_in_chat() { // Obtiene un listado de los usuarios y su informacion
-
+void recv_msg_handler() {
+	char message[LENGTH] = {};
+  while (1) {
+		int receive = recv(sockfd, message, LENGTH, 0);
+    if (receive > 0) {
+      printf("%s", message);
+      str_overwrite_stdout();
+    } else if (receive == 0) {
+			break;
+    } else {
+			// -1
+		}
+		memset(message, 0, sizeof(message));
+  }
 }
 
-int main () {
+int main(int argc, char **argv){
+	if(argc != 2){
+		printf("Uso: %s <puerto>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
+	char *ip = "127.0.0.1";
+	int port = atoi(argv[1]);
+
+	signal(SIGINT, catch_ctrl_c_and_exit);
+
+	printf("Cual es tu nombre?: ");
+  fgets(name, 32, stdin);
+  str_trim_lf(name, strlen(name));
+
+
+	if (strlen(name) > 32 || strlen(name) < 2){
+		printf("El nombre debe tener una longitud de maxima de 30 caracteres y minima de 2 caracteres\n");
+		return EXIT_FAILURE;
+	}
+
+	struct sockaddr_in server_addr;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+  server_addr.sin_port = htons(port);
+
+
+  // Conectar al server
+  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (err == -1) {
+		printf("ERROR: conectar al server\n");
+		return EXIT_FAILURE;
+	}
+
+	// Send name
+	send(sockfd, name, 32, 0);
+
+	printf("!--- Bienvenidos a La Cueva ---!\n");
+
+	pthread_t send_msg_thread;
+  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+    return EXIT_FAILURE;
+	}
+
+	pthread_t recv_msg_thread;
+  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+		return EXIT_FAILURE;
+	}
+
+	while (1){
+		if(flag){
+			printf("\nNos vemos!\n");
+			break;
+    }
+	}
+
+	close(sockfd);
+
+	return EXIT_SUCCESS;
 }
--- server.c
+++ server.c
@@ -1,36 +1,229 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+//#include <chat.pb-c.h>
 
-void global_message_handler() { // Maneja los mensajes globales enviados a todos los clientes
 
+#define MAX_CLIENTS 10
+#define BUFFER_SZ 2048
 
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
+
+// Estructura del cliente
+typedef struct{
+	struct sockaddr_in address;
+	int sockfd;
+	int uid;
+	char name[32];
+} client_t;
+
+client_t *clients[MAX_CLIENTS]; // Array de clientes
+
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
 }
 
-void client_handle() { // Maneja a los clientes
-
-
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { 
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
 }
 
-void register_user() { // Maneja el registro de los clientes
-
-
+void print_client_addr(struct sockaddr_in addr){
+    printf("%d.%d.%d.%d",
+        addr.sin_addr.s_addr & 0xff,
+        (addr.sin_addr.s_addr & 0xff00) >> 8,
+        (addr.sin_addr.s_addr & 0xff0000) >> 16,
+        (addr.sin_addr.s_addr & 0xff000000) >> 24);
 }
 
-void free_user() { // Liberacion de usuarios
+// Registro de usuarios
+void queue_add(client_t *cl){
+	pthread_mutex_lock(&clients_mutex);
 
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(!clients[i]){
+			clients[i] = cl;
+			break;
+		}
+	}
 
+	pthread_mutex_unlock(&clients_mutex);
 }
 
-int main() {
+// Liberacion de usuarios
+void queue_remove(int uid){
+	pthread_mutex_lock(&clients_mutex);
 
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(clients[i]){
+			if(clients[i]->uid == uid){
+				clients[i] = NULL;
+				break;
+			}
+		}
+	}
 
+	pthread_mutex_unlock(&clients_mutex);
 }
 
+//Mensaje global
+void send_message(char *s, int uid){
+	pthread_mutex_lock(&clients_mutex);
+
+	for(int i=0; i<MAX_CLIENTS; ++i){
+		if(clients[i]){
+			if(clients[i]->uid != uid){
+				if(write(clients[i]->sockfd, s, strlen(s)) < 0){
+					perror("ERROR: escritura fallida");
+					break;
+				}
+			}
+		}
+	}
+
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+// Manejo de la comunicacion con el cliente
+void *handle_client(void *arg){
+	char buff_out[BUFFER_SZ];
+	char name[32];
+	int leave_flag = 0;
+
+	cli_count++;
+	client_t *cli = (client_t *)arg;
+
+	// Name
+	if(recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) <  2 || strlen(name) >= 32-1){
+		printf("Nombre invalido.\n");
+		leave_flag = 1;
+	} else{
+		strcpy(cli->name, name);
+		sprintf(buff_out, "%s se ha unido!\n", cli->name);
+		printf("%s", buff_out);
+		send_message(buff_out, cli->uid);
+	}
+
+	bzero(buff_out, BUFFER_SZ);
+
+	while(1){
+		if (leave_flag) {
+			break;
+		}
+
+		int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
+		if (receive > 0){
+			if(strlen(buff_out) > 0){
+				send_message(buff_out, cli->uid);
+
+				str_trim_lf(buff_out, strlen(buff_out));
+				printf("%s -> %s\n", buff_out, cli->name);
+			}
+		} else if (receive == 0 || strcmp(buff_out, "exit") == 0){
+			sprintf(buff_out, "%s se ha pirado.\n", cli->name);
+			printf("%s", buff_out);
+			send_message(buff_out, cli->uid);
+			leave_flag = 1;
+		} else {
+			printf("ERROR: -1\n");
+			leave_flag = 1;
+		}
+
+		bzero(buff_out, BUFFER_SZ);
+	}
+
+  // Liberar usuario y thread
+	close(cli->sockfd);
+  queue_remove(cli->uid);
+  free(cli);
+  cli_count--;
+  pthread_detach(pthread_self());
+
+	return NULL;
+}
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		printf("Uso: %s <puerto>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char *ip = "127.0.0.1";
+	int port = atoi(argv[1]);
+	int option = 1;
+	int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in cli_addr;
+  pthread_t tid;
+
+  // Settings del socket
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(ip);
+  serv_addr.sin_port = htons(port);
+
+	signal(SIGPIPE, SIG_IGN);
+
+	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
+		perror("ERROR: setsockopt fallido");
+    return EXIT_FAILURE;
+	}
+
+  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("ERROR: Bindeo del socket fallido");
+    return EXIT_FAILURE;
+  }
+
+  if (listen(listenfd, 10) < 0) {
+    perror("ERROR: Fallo al escuchar al socket");
+    return EXIT_FAILURE;
+	}
+
+	printf("!--- Bienvenidos a La Cueva ---!\n");
+
+	while(1){
+		socklen_t clilen = sizeof(cli_addr);
+		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+
+		//Chequeo de maximo de clientes
+		if((cli_count + 1) == MAX_CLIENTS){
+			printf("Capacidad del clientes excedida ");
+			print_client_addr(cli_addr);
+			printf(":%d\n", cli_addr.sin_port);
+			close(connfd);
+			continue;
+		}
+
+		// Ajustes del cliente
+		client_t *cli = (client_t *)malloc(sizeof(client_t));
+		cli->address = cli_addr;
+		cli->sockfd = connfd;
+		cli->uid = uid++;
+
+		//Añadir cliente a la fila
+		queue_add(cli);
+		pthread_create(&tid, NULL, &handle_client, (void*)cli);
+
+		// Optimizacion para que Linux no pete.
+		sleep(1);
+	}
+
+	return EXIT_SUCCESS;
+}
